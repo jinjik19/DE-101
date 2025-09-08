447d002af0a39e536f234b9c56989a64f29d2e8f
Evgeniy Krasnikov <e.krasnikov@runity.ru>
2025-09-08 18:27:45 +0700

    add new module files

--- Module2/sql_scripts/stg/dashboard.sql
+++ Module2/sql_scripts/stg/dashboard.sql
@@ -1,11 +1,26 @@
---- KPI
+--- sales and profit dynamic
 select
-	TO_CHAR(order_date, 'YYYY-MM') as order_date_year_month,
 	ROUND(SUM(sales), 2) as sum_sales_by_month,
 	ROUND(SUM(profit), 2) as sum_profit_by_month,
-	ROUND(AVG(discount) * 100, 2) as avg_discount_by_month,
-	ROUND((SUM(profit) / SUM(sales)) * 100, 2) as profit_ration_by_month
 from stg.order
 group by order_date_year_month
 order by order_date_year_month ASC
+---
+
+--- KPI
+select
+	ROUND(SUM(sales), 2) as total_sales,
+	ROUND(SUM(profit), 2) as total_profit,
+	ROUND(AVG(discount) * 100, 2) as avg_discount,
+	ROUND((SUM(profit) / SUM(sales)) * 100, 2) as profit_ratio
+from stg.order
+---
+
+--- sales and profit by categories
+select
+	category,
+	ROUND(SUM(sales), 2) as total_sales,
+	ROUND(SUM(profit), 2) as total_profit
+from stg.order
+group by category
 ---
